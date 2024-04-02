f3cfe3786e744f6d3dda49fe3915e55697a0ae06
cavefish <cavefish@gmail.com>
2024-04-02 11:30:20 +0200

    FIX update dependencies and make service runnable to last java version

---
+++ doc/changelog.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 1
+
+- Update dependencies to last version to avoid vulnerabilities
+- Change code to be able to compile under new dependency versions
+- Create get request to test that the spring controller is running
+
--- src/main/java/com/playtomic/tests/wallet/service/StripeRestTemplateResponseErrorHandler.java
+++ src/main/java/com/playtomic/tests/wallet/service/StripeRestTemplateResponseErrorHandler.java
@@ -1,7 +1,9 @@
 package com.playtomic.tests.wallet.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
 import java.io.IOException;
@@ -9,7 +11,7 @@ import java.io.IOException;
 public class StripeRestTemplateResponseErrorHandler extends DefaultResponseErrorHandler {
 
     @Override
-    protected void handleError(ClientHttpResponse response, HttpStatus statusCode) throws IOException {
+    protected void handleError(@NonNull ClientHttpResponse response, @NonNull HttpStatusCode statusCode) throws IOException {
         if (statusCode == HttpStatus.UNPROCESSABLE_ENTITY) {
             throw new StripeAmountTooSmallException();
         }
